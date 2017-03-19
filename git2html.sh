4216957bcb86e848b65982eaa4e3a0cb827a3817
Chris Warburton <chriswarbo@gmail.com>
2017-03-19 10:09:23 +0000

    Shallow clone

--- git2html.sh
+++ git2html.sh
@@ -187,7 +187,8 @@ unset GIT_DIR
 if test ! -e "$TARGET/repository"
 then
   # Clone the repository.
-  git clone --recursive "$REPOSITORY" "$TARGET/repository"
+  git clone --depth 1 --shallow-submodules --recursive "$REPOSITORY" \
+            "$TARGET/repository"
   cd "$TARGET/repository"
 
   # We don't need any local branches.  In fact, they only make trouble
